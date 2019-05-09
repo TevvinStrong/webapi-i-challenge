6c0c7a5086802f504802985ccf1086fd264da157
Tevvin <Tevvin7395@icloud.com>
2019-05-08 21:18:45 -0400

    Basic setup initial commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+const server = express();
+
+// Middleware
+server.use(express.json());
+
+//*** Endpoints ***\\
+
+// Test  
+server.get('/api', (req, res) => {
+    res.send('<h1>This is a test</h1>');
+});
+
+// Create
+
+
+
+
+// Delete
+
+
+
+
+// Put - Update
+
+
+
+
+// Listerning
+server.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
